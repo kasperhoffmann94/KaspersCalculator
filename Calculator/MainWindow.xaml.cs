3865be1c37a539330ba898762ca371d05975661b
Kasper <kasperhoffmann@hotmail.dk>
2020-01-25 18:33:59 +0100

    Add method for doing calculations and modified code so it is more neat to look at

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -27,33 +27,67 @@ namespace Calculator
 
         private void Addition_ButtonClicked(object sender, RoutedEventArgs e)
         {
-           
-           Result.Text = (int.Parse(Number1.Text) + int.Parse(Number2.Text)).ToString();
+
+             CalculateWithOperator(0);
         }
 
         private void Subtraction_ButtonClicked(object sender, RoutedEventArgs e)
         {
-            Result.Text = (int.Parse(Number1.Text) - int.Parse(Number2.Text)).ToString();
+            CalculateWithOperator(1);
         }
 
         private void Multiply_ButtonClicked(object sender, RoutedEventArgs e)
         {
-            Result.Text = (int.Parse(Number1.Text) * int.Parse(Number2.Text)).ToString();
+            CalculateWithOperator(2);
         }
 
         private void Divide_ButtonClicked(object sender, RoutedEventArgs e)
         {
-            Result.Text = (int.Parse(Number1.Text) / int.Parse(Number2.Text)).ToString();
+            CalculateWithOperator(3);
         }
 
         private void Modulus_ButtonClicked(object sender, RoutedEventArgs e)
         {
-
+            CalculateWithOperator(4);
         }
 
         private void SquareRoot_ButtonClicked(object sender, RoutedEventArgs e)
         {
 
+        }
+
+
+        private void CalculateWithOperator(int operatorInt)
+        {
+            int firstInput = int.Parse(Number1_Textbox.Text);
+            int secondInput = int.Parse(Number2_Textbox.Text);
+            int result = 0;
+            switch (operatorInt)
+            {
+                case 0:
+                    result = firstInput + secondInput;
+                    break;
+                case 1:
+                    result = firstInput - secondInput;
+                    break;
+                case 2:
+                    result = firstInput * secondInput;
+                    break;
+                case 3:
+                    result = firstInput / secondInput;
+                    break;
+                case 4:
+                    result = firstInput % secondInput;
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+            }
+
+            Result_Textbox.Text = result.ToString();
         }
     }
 }
